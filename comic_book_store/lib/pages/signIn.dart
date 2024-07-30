428b71e89fd85d2f7a8b4d83a0bc60bd3bfe1b64
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-07-30 21:31:33 +0530

    chore: Add SignIn page for user authentication

--- comic_book_store/lib/pages/signIn.dart
+++ comic_book_store/lib/pages/signIn.dart
@@ -1,1 +1,65 @@
+import 'package:comic_book_store/components/button.dart';
+import 'package:comic_book_store/components/input.dart';
+import 'package:comic_book_store/components/theme.dart';
+import 'package:flutter/material.dart';
 
+class SingIn extends StatefulWidget {
+  const SingIn({super.key});
+
+  @override
+  State<SingIn> createState() => _SingInState();
+}
+
+class _SingInState extends State<SingIn> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Template(
+      screenWidth: screenWidth,
+      screenHeight: screenHeight,
+      theChild: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: screenHeight * 0.2),
+            const Text(
+              "Sing in now",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.003),
+            const Text(
+              "Please sing in to continue our app",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.04),
+            CustomInputField(hintText: "Email", controller: _emailController),
+            SizedBox(height: screenHeight * 0.04),
+            CustomInputField(
+                hintText: "Password", controller: _passwordController),
+            SizedBox(height: screenHeight * 0.02),
+            SizedBox(height: screenHeight * 0.02),
+            CustomButton(
+              height: screenHeight * 0.08,
+              width: screenWidth * 1,
+              text: "Sing in",
+              onPressed: () {
+                print("Sing in");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
