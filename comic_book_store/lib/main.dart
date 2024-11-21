57ad3e0baac75bce2b102ef7ecf9622689458786
RMCV-Rajapaksha <cam.chamara@gmail.com>
2024-11-22 00:28:26 +0530

    Add Firebase integration and configuration for the app

--- comic_book_store/lib/main.dart
+++ comic_book_store/lib/main.dart
@@ -11,8 +11,14 @@ import 'package:comic_book_store/pages/welcome.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Add this import statement
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     DevicePreview(
       enabled: true,
