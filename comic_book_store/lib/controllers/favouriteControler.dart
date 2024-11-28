1456724806cda70fa660e3ecb99dc00b404a563d
RMCV-Rajapaksha <cam.chamara@gmail.com>
2024-11-28 17:16:07 +0530

    Enhance Manga model with JSON serialization; update favorite management to associate with user accounts and improve UI in intro pages

--- comic_book_store/lib/controllers/favouriteControler.dart
+++ comic_book_store/lib/controllers/favouriteControler.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:comic_book_store/api/jikan_service.dart';
 import 'package:comic_book_store/models/manga.dart';
-
-import '../api/jikan_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MangaController {
   final JikanService _jikanService = JikanService();
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<Manga?> fetchMangaDetails(int mangaId) async {
     try {
@@ -19,8 +20,13 @@ class MangaController {
 
   Future<bool> isFavorite(int mangaId) async {
     try {
+      final user = _auth.currentUser;
+      if (user == null) return false;
+
       final favoriteDoc = await _firestore
           .collection('favorites')
+          .doc(user.email)
+          .collection('userFavorites')
           .doc(mangaId.toString())
           .get();
       return favoriteDoc.exists;
@@ -32,18 +38,25 @@ class MangaController {
 
   Future<void> toggleFavorite(Manga manga, bool isFavorite) async {
     try {
-      final favoriteRef =
-          _firestore.collection('favorites').doc(manga.malId.toString());
+      final user = _auth.currentUser;
+      if (user == null) return;
+
+      final favoriteRef = _firestore
+          .collection('favorites')
+          .doc(user.email)
+          .collection('userFavorites')
+          .doc(manga.malId.toString());
 
       if (isFavorite) {
         // Remove from favorites
         await favoriteRef.delete();
       } else {
         // Add to favorites
-        // await favoriteRef.set({
-        //   ...manga.toJson(),
-        //   'addedAt': FieldValue.serverTimestamp(),
-        // });
+        await favoriteRef.set({
+          'userEmail': user.email,
+          ...manga.toJson(),
+          'addedAt': FieldValue.serverTimestamp(),
+        });
       }
     } catch (e) {
       print('Error toggling favorite: $e');
--- comic_book_store/lib/pages/intro_pages/page2.dart
+++ comic_book_store/lib/pages/intro_pages/page2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:comic_book_store/constants/colors.dart';
 
 class Intro_page2 extends StatelessWidget {
   const Intro_page2({super.key});
@@ -8,41 +9,52 @@ class Intro_page2 extends StatelessWidget {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: screenHeight * 0.1),
-            Center(
-              child: Column(
-                children: [
-                  Image.asset(
-                    'assets/images/page_02.png',
-                    width: screenWidth,
-                  ),
-                ],
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: screenHeight * 0.1),
+              Center(
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(24.0),
+                      child: Image.asset(
+                        'assets/images/page_02.png',
+                        width: screenWidth * 0.8,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.04),
+                    const Text(
+                      "Your Personal Comic Library",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 24,
+                        color: AppColors.accent2,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    const Text(
+                      "Build your ultimate collection with our easy-to-use store. Buy, sell, and manage your comics all in one place. Whether you're a casual reader or a hardcore collector, our app has everything you need to fuel your passion.",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 17,
+                        color: AppColors.accent2,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const Text(
-              "Your Personal Comic Library ",
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 24,
-              ),
-            ),
-            const Text(
-              "Build your ultimate collection with our easy-to-use store. Buy, sell, and manage your comics all in one place. Whether you're a casual reader or a hardcore collector, our app has everything you need to fuel your passion.",
-              style: TextStyle(
-                fontWeight: FontWeight.w300,
-                fontSize: 17,
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
